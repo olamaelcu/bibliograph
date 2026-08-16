be2987f493684023f10f60cd1ff6901757c26c6b
Jacky Alcine <yo@jacky.wtf>
2026-08-16 18:15:10 -0400

    chore(db): switch to pg driver and postgresql dialect config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  dialect: 'sqlite',
+  dialect: 'postgresql',
   schema: './src/db/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DB_PATH
+    url: process.env.DATABASE_URL
   },
 });
