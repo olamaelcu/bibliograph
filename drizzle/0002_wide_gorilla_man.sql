90a4d960d474aea01bdb3ccd8a3b5c21d465caef
Jacky Alcine <yo@jacky.wtf>
2026-08-17 10:13:53 -0400

    make a fix about integers and backfills

---
+++ drizzle/0002_wide_gorilla_man.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "backfill_state" ALTER COLUMN "file_size" SET DATA TYPE bigint;--> statement-breakpoint
+ALTER TABLE "backfill_state" ALTER COLUMN "last_byte_offset" SET DATA TYPE bigint;
